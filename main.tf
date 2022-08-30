d751cf82f4cc8d10946aa0740debc40eea394a68
anand944 <anandkumarkumbaji07@gmail.com>
2022-08-30 15:07:50 +0530

    change vnet

--- main.tf
+++ main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "resorcegroup" {
   name     = "rm07"
   location = "East us"
 }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet01"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
